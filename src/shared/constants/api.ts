197ebb97a0ea07454d9a2300e80231b07bc65ebc
saltilo <115881786+saltilo@users.noreply.github.com>
2025-07-15 00:18:28 +0300

    fix: api path

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3001"
-    : "https://lingua-app-qmb9.onrender.com";
+    : "https://lingua-app-qmb9.onrender.com/api";
